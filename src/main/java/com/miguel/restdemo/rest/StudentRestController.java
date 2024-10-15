5cae719de0c7bba428fcd46ad36bd58e3a6633e3
Miguel Montagner <dev@miguelmontagner.com>
2024-10-15 18:39:55 +0100

    added rest controller for student

---
+++ src/main/java/com/miguel/restdemo/rest/StudentRestController.java
@@ -0,0 +1,27 @@
+package com.miguel.restdemo.rest;
+
+import com.miguel.restdemo.entity.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    //define endpoint for "/students" - return a list of students
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+
+        List<Student> theStudents = new ArrayList<>();
+
+        theStudents.add(new Student("Foo","Bar"));
+        theStudents.add(new Student("Tom","Jerry"));
+        theStudents.add(new Student("Herp","Derp"));
+        return theStudents;
+    }
+}
