b8fc4da4ef668b332fd01d7eb6ae0ac017c75727
Miguel Montagner <dev@miguelmontagner.com>
2024-10-16 03:57:10 +0100

    added catch all exception handling

--- src/main/java/com/miguel/restdemo/rest/StudentRestController.java
+++ src/main/java/com/miguel/restdemo/rest/StudentRestController.java
@@ -61,4 +61,19 @@ public class StudentRestController {
 
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
+
+    //add exception handler catch all
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
+
+        //create a StudentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+        //return response entity
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
 }
