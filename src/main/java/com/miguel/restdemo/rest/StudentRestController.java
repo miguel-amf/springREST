8b50163144079c5baee6c60c2746853c9fc02711
Miguel Montagner <dev@miguelmontagner.com>
2024-10-16 03:07:47 +0100

    added postconstruct to populate data

--- src/main/java/com/miguel/restdemo/rest/StudentRestController.java
+++ src/main/java/com/miguel/restdemo/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.miguel.restdemo.rest;
 
 import com.miguel.restdemo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,16 +13,21 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    //define endpoint for "/students" - return a list of students
-
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-
-        List<Student> theStudents = new ArrayList<>();
+    private List<Student> theStudents;
+    //define @PostConstruct
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
 
         theStudents.add(new Student("Foo","Bar"));
         theStudents.add(new Student("Tom","Jerry"));
         theStudents.add(new Student("Herp","Derp"));
+    }
+    //define endpoint for "/students" - return a list of students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+
+
         return theStudents;
     }
 }
